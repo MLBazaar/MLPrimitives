68dd0264cc8043b2fc714863b8d9c641696132b5
Carles Sala <c.sala.stq@gmail.com>
2018-08-17 19:21:55 +0200

    Bump version: 0.1.0-dev → 0.1.0

--- setup.py
+++ setup.py
@@ -104,6 +104,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLPrimitives',
-    version='0.1.0-dev',
+    version='0.1.0',
     zip_safe=False,
 )
