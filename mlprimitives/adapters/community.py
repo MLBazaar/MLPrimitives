93849188ea247ea6e4535295b7bfb15c647a0b4a
Carles Sala <c.sala.stq@gmail.com>
2018-08-17 15:13:25 +0200

    Add defaults to community args

--- mlprimitives/adapters/community.py
+++ mlprimitives/adapters/community.py
@@ -7,9 +7,9 @@ import pandas as pd
 
 class CommunityBestPartition(object):
 
-    def produce(self, X, partition=None, graph=None):
-        partition = partition or co.best_partition(graph)
-        values = [b for a, b in partition.items()]
+    def produce(self, X, best_partition=None, graph=None):
+        best_partition = best_partition or co.best_partition(graph)
+        values = [b for a, b in best_partition.items()]
         missing_community_index = np.max(values) + 10
 
         result = pd.Series(index=X.index)
@@ -17,11 +17,11 @@ class CommunityBestPartition(object):
         for i in X.index:
             node = X.loc[i][0]
 
-            if node in partition:
-                community = partition[node]
+            if node in best_partition:
+                community = best_partition[node]
 
-            elif str(node) in partition:
-                community = partition[str(node)]
+            elif str(node) in best_partition:
+                community = best_partition[str(node)]
 
             else:
                 community = missing_community_index
