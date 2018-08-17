e734b01653dacf5491f9b1460e175878fee4b122
Carles Sala <c.sala.stq@gmail.com>
2018-08-17 15:12:34 +0200

    Move networkx helper functions to its own adapter module

--- mlprimitives/utils.py
+++ mlprimitives/utils.py
@@ -5,7 +5,6 @@ import logging
 import math
 
 import numpy as np
-import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 
@@ -59,52 +58,3 @@ def image_transform(X, function, reshape_before=False, reshape_after=False,
         return features
 
     return np.apply_along_axis(apply_function, axis=1, arr=X)
-
-
-def graph_pairs_feature_extraction(X, functions, node_columns, graphs=None):
-    functions = [import_object(function) for function in functions]
-
-    pairs = X[node_columns].values
-
-    for i, graph in enumerate(graphs):
-        def apply(function):
-            try:
-                values = function(graph, pairs)
-                return np.array(list(values))[:, 2]
-
-            except ZeroDivisionError:
-                LOGGER.warn("ZeroDivisionError captured running %s", function)
-                return np.zeros(len(pairs))
-
-        for function in functions:
-            name = '{}_{}'.format(function.__name__, i)
-            X[name] = apply(function)
-
-    return X
-
-
-def graph_feature_extraction(X, functions, graphs):
-    functions = [import_object(function) for function in functions]
-
-    for node_column, graph in graphs.items():
-        index_type = type(X[node_column].values[0])
-
-        features = pd.DataFrame(index=graph.nodes)
-        features.index = features.index.astype(index_type)
-
-        def apply(function):
-            values = function(graph)
-            return np.array(list(values.values()))
-
-        for function in functions:
-            name = '{}_{}'.format(function.__name__, node_column)
-            features[name] = apply(function)
-
-        X = X.merge(features, left_on=node_column, right_index=True, how='left')
-
-        graph_data = pd.DataFrame(dict(graph.nodes.items())).T
-        graph_data.index = graph_data.index.astype(index_type)
-
-        X = X.merge(graph_data, left_on=node_column, right_index=True, how='left')
-
-    return X
