ea3f329542f77c781279c3f694386b0ffa7fcf33
Carles Sala <carles@pythiac.com>
2018-10-05 19:32:32 -0400

    Remove pipeline execution during tests

--- tests/test_mlblocks_primitives.py
+++ tests/test_mlblocks_primitives.py
@@ -6,7 +6,6 @@ from unittest.mock import patch
 
 import pandas as pd
 from mlblocks import MLPipeline
-from sklearn import datasets
 
 PRIMITIVES_PATH = os.path.normpath(
     os.path.join(
@@ -23,15 +22,6 @@ HYPERPARAMETER_DEFAULTS = {
     'bool': True,
     'list': [],
     'dict': dict(),
-}
-
-BOSTON = datasets.load_boston()
-DIGITS = datasets.load_digits()
-WINE = datasets.load_wine()
-DATASETS = {
-    'boston': (pd.DataFrame(BOSTON.data), pd.Series(BOSTON.target)),
-    'digits': (pd.DataFrame(DIGITS.data), pd.Series(DIGITS.target)),
-    'wine': (pd.DataFrame(WINE.data), pd.Series(WINE.target)),
 }
 
 
@@ -67,13 +57,6 @@ def test_jsons():
 
                 produce = primitive['produce']
                 assert hasattr(mlblock.instance, produce['method'])
-
-            # Run pipeline, when possible
-            validation_dataset = primitive.get('validation_dataset')
-            if validation_dataset:
-                X, y = DATASETS[validation_dataset]
-                mlpipeline.fit(X, y)
-                mlpipeline.predict(X)
 
         except Exception:
             raise ValueError("Invalid JSON primitive: {}".format(primitive_filename))
