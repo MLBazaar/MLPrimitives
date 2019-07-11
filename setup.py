8dfea5472b660d93392685e737644b33df1c5b3c
Carles Sala <carles@pythiac.com>
2019-07-11 16:17:28 -0400

    Add Release notes for v0.2.0

--- setup.py
+++ setup.py
@@ -127,6 +127,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLPrimitives',
-    version='0.1.11-dev',
+    version='0.2.0-dev',
     zip_safe=False,
 )
