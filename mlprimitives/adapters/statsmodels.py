c3e79befb46ef7a2e45c765064603069757b2be2
Alexander Geiger <alex.geiger95@gmx.de>
2019-08-12 10:33:47 -0400

    add docstrings, add unit tests

--- mlprimitives/adapters/statsmodels.py
+++ mlprimitives/adapters/statsmodels.py
@@ -3,20 +3,56 @@ from statsmodels.tsa import arima_model
 
 
 class ARIMA(object):
-    """A Wrapper for the statsmodels ARIMA model for time series predictions"""
+    """A Wrapper for the statsmodels.tsa.arima_model.ARIMA class.
+    """
 
     def __init__(self, p, d, q, steps):
+        """Initialize the ARIMA object.
+
+        Args:
+            p (int):
+                Integer denoting the order of the autoregressive model.
+            d (int):
+                Integer denoting the degree of differencing.
+            q (int):
+                Integer denoting the order of the moving-average model.
+            steps (int):
+                Integer denoting the number of time steps to predict ahead.
+        """
         self.p = p
         self.d = d
         self.q = q
         self.steps = steps
 
     def predict(self, X):
-        y = list()
-        if len(X.shape) == 1 or len(X.shape) == 2 and X.shape[1] == 1:
+        """Predict values using the initialized object.
+
+        Args:
+            X (ndarray):
+                N-dimensional array containing the input sequences for the model.
+
+        Returns:
+            ndarray:
+                N-dimensional array containing the predictions for each input sequence.
+        """
+        arima_results = list()
+        dimensions = len(X.shape)
+
+        if dimensions > 2:
+            raise ValueError("Only 1D o 2D arrays are supported")
+
+        if dimensions == 1 or X.shape[1] == 1:
             X = np.expand_dims(X, axis=0)
-        for i in range(len(X)):
-            model = arima_model.ARIMA(X[i], order=(self.p, self.d, self.q))
-            model = model.fit(disp=0)
-            y.append(model.forecast(self.steps)[0])
-        return np.asarray(y)
+
+        num_sequences = len(X)
+        for sequence in range(num_sequences):
+            arima = arima_model.ARIMA(X[sequence], order=(self.p, self.d, self.q))
+            arima_fit = arima.fit(disp=0)
+            arima_results.append(arima_fit.forecast(self.steps)[0])
+
+        arima_results = np.asarray(arima_results)
+
+        if dimensions == 1:
+            arima_results = arima_results[0]
+
+        return arima_results
