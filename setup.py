c0472ae8c4835d385f0dc6a02ba9b93099efb43c
Carles Sala <carles@pythiac.com>
2020-07-28 16:41:25 +0200

    Bump version: 0.2.5.dev0 → 0.2.5.dev1

--- setup.py
+++ setup.py
@@ -125,6 +125,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLPrimitives',
-    version='0.2.5.dev0',
+    version='0.2.5.dev1',
     zip_safe=False,
 )
