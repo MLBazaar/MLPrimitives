d32cab0aa1d4d803149db7ae0803b2a0b5dc8199
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-12-21 16:13:18 +0100

    Bump version: 0.2.6.dev1 → 0.3.0.dev0

--- setup.py
+++ setup.py
@@ -127,6 +127,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLPrimitives',
-    version='0.2.6.dev1',
+    version='0.3.0.dev0',
     zip_safe=False,
 )
