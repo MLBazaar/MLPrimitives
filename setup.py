5fab607f44b50e81887ba3f24bdc84089bdd18f8
Carles Sala <carles@pythiac.com>
2021-01-09 16:45:42 +0100

    Bump version: 0.3.0.dev2 → 0.3.0

--- setup.py
+++ setup.py
@@ -117,6 +117,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/MLBazaar/MLPrimitives',
-    version='0.3.0.dev2',
+    version='0.3.0',
     zip_safe=False,
 )
