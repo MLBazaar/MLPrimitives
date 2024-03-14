2e370ae22b1dbbc4f940869364543c2ab7374291
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2024-03-14 16:38:46 -0400

    Bump version: 0.3.6.dev0 → 0.3.6.dev1

--- setup.py
+++ setup.py
@@ -132,6 +132,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/MLBazaar/MLPrimitives',
-    version='0.3.6.dev0',
+    version='0.3.6.dev1',
     zip_safe=False,
 )
