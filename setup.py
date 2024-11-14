39c1726b4c528a07443bab87dddde92b7a54cfbf
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2024-11-14 17:34:13 -0500

    update python version and packages

--- setup.py
+++ setup.py
@@ -12,23 +12,23 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    'Keras>=2.4,<2.15',
-    'featuretools>=0.6.1,<0.23',
+    'Keras>=2.4,<4',
+    'featuretools>=0.6.1,<2',
     'iso639>=0.1.4,<0.2',
     'langdetect>=1.0.7,<2',
-    'lightfm>=1.15,<2',
-    'mlblocks>=0.6,<0.7',
+    #'lightfm>=1.15,<2', <-- no longer supported
+    'mlblocks>=0.6,<1',
     'networkx>=2.0,<3',
     'nltk>=3.3,<4',
-    'numpy>=1.16.0,<2',
+    'numpy>=1.16.0,<3',
     'opencv-python>=3.4.0.12,<4.7',
-    'pandas>=1,<2',
+    'pandas>=1,<3',
     'python-louvain>=0.10,<0.14',   # community
     'scikit-image>=0.15',
     'scikit-learn>=0.21',
     'scipy>=1.1.0,<2',
     'statsmodels>=0.9.0,<0.15',
-    'tensorflow>=2,<2.15',
+    'tensorflow>=2,<3',
     'xgboost>=0.72.1,<2',
     'protobuf<4',
 ]
@@ -48,7 +48,7 @@ development_requires = [
     # general
     'bumpversion>=0.5.3,<0.6',
     'pip>=9.0.1',
-    'watchdog>=0.8.3,<0.11',
+    'watchdog>=0.8.3,<5',
 
     # docs
     'm2r>=0.2.0,<0.3',
@@ -104,6 +104,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     description='Pipelines and primitives for machine learning and data science.',
     entry_points = {
@@ -127,7 +128,7 @@ setup(
     long_description_content_type='text/markdown',
     name='mlprimitives',
     packages=find_packages(include=['mlprimitives', 'mlprimitives.*']),
-    python_requires='>=3.8,<3.12',
+    python_requires='>=3.8,<3.13',
     setup_requires=setup_requires,
     test_suite='tests',
     tests_require=tests_require,
